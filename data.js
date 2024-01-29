b70673bb5dd46d053716f5c7d941a8a235b29d30
Anas Nur Khafid <anasn.khafid@gmail.com>
2024-01-29 17:02:49 +0700

    Firs Commit

---
+++ data.js
@@ -0,0 +1,27 @@
+const nama ="Anas Nur Khafid";
+let usia = 10
+
+let biodata = document.getElementById('biodata');
+
+
+function generateBiodata() { 
+    let generasi;
+
+    if ( usia > 10 && usia < 18 ) {
+        generasi = " Generasi Remaja "
+    } else if ( usia > 18 && usia < 30) {
+        generasi = " Generasi Dewasa "
+    }  else if ( usia >= 30) {
+        generasi = " Generasi Tua "
+    }  else if ( usia > 2 && usia < 10) {
+        generasi = " Generasi Anak-anak "
+    } else {
+        generasi = " Generasi Bayi"
+    }
+    return biodata.innerHTML = generasi;
+}
+
+console.log(nama);
+console.log(usia);
+
+generateBiodata();
